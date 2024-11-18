90c6cac6a59690298f397174332fdb86d1344691
Ian Fisk <i.fisk32@gmail.com>
2024-11-18 10:54:06 -0700

    Code review activity: Update vowel-counter.py after third review session.

--- code-reviews/vowel-counter/vowel-counter.py
+++ code-reviews/vowel-counter/vowel-counter.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
 VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-def count_vowels(text):
+def count_characters(text, charsToCount):
 	if text is None:
 		raise ValueError("'text' argument must be provided")
-
-	count = 0
-	for vowel in VOWELS:
-		count += text.count(vowel)
-	return count
-
-def count_consonants(text):
-	if text is None:
-		raise ValueError("'text' argument must be provided")
+	if charsToCount is None:
+		raise ValueError("'charsToCount' argument must be provided")
 
 	count = 0
 	for char in text:
-		if char not in VOWELS:
+		if char in charsToCount:
 			count += 1
 	return count
+
+def count_vowels(text):
+	return count_characters(text, VOWELS)
+
+def count_consonants(text):
+	return count_characters(text, CONSONANTS)
 
 sentence = input("Enter a sentence: ")
 print(f"Number of vowels {count_vowels(sentence)}")
