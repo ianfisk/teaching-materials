7395d18c809d4954eb63c9376855003221102fa8
Ian Fisk <i.fisk32@gmail.com>
2024-11-18 10:41:47 -0700

    Code review activity: add revision 3

---
+++ code-reviews/vowel-counter/vowel-counter-revision3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+def count_characters(text, charsToCount):
+	if text is None:
+		raise ValueError("'text' argument must be provided")
+	if charsToCount is None:
+		raise ValueError("'charsToCount' argument must be provided")
+
+	count = 0
+	for char in text:
+		if char in charsToCount:
+			count += 1
+	return count
+
+def count_vowels(text):
+	return count_characters(text, VOWELS)
+
+def count_consonants(text):
+	return count_characters(text, CONSONANTS)
+
+sentence = input("Enter a sentence: ")
+print(f"Number of vowels {count_vowels(sentence)}")
+print(f"Number of consonants {count_consonants(sentence)}")
